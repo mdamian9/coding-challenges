984da089cfca7dffed27b8b9ba5c6886a4504486
mdamian9 <m.damian9@hotmail.com>
2018-11-16 13:53:01 -0800

    Added function to check if letter is vowel did some testing with convert function

--- PigLatinConverter.js
+++ PigLatinConverter.js
@@ -1,10 +1,36 @@
-const vowels = ["a", "e", "i", "o", "u"];
-const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+const vowels = ["a", "e", "i", "o", "u", "y"];
+const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"];
 
-convert = (word) => {
-
+isVowel = (letter) => {
+    let flag = false;
+    for (let i = 0; i < vowels.length; i++) {
+        if (letter === vowels[i]) {
+            flag = true;
+        };
+    };
+    return flag;
 };
 
-console.log(convert("say"));
-console.log(convert("english"));
-console.log(convert("smile"));
+convert = (word) => {
+    let vowCluster = "", conCluster = "";
+    for (let i = 0; i < word.length; i++) {
+        console.log(word[i]);
+        for (let j = 0; j < vowels.length; j++) {
+            if (word[i] === vowels[j]) {
+                console.log(`${word[i]} = ${vowels[j]}`);
+                vowCluster += word[i];
+            };
+        };
+    };
+
+    if (word)
+
+    console.log(vowCluster);
+
+    // return word;
+};
+
+// console.log(convert("say"));
+console.log(isVowel("t"));
+// console.log(convert("english"));
+// console.log(convert("smile"));
