63fe46655482f15e8de31ea95c7bfd685c59dc63
mdamian9 <m.damian9@hotmail.com>
2018-11-16 16:25:23 -0800

    Rearranged code, did some testing

--- PigLatinConverter.js
+++ PigLatinConverter.js
@@ -1,36 +1,42 @@
 const vowels = ["a", "e", "i", "o", "u", "y"];
 const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"];
 
-isVowel = (letter) => {
-    let flag = false;
+convert = (word) => {
+
+    let isVowel, isConsonant, vowCluster = "", conCluster = "", pigLatinWord = "";
     for (let i = 0; i < vowels.length; i++) {
-        if (letter === vowels[i]) {
-            flag = true;
+        if (word[0] === vowels[i]) {
+            isVowel = true;
         };
     };
-    return flag;
-};
 
-convert = (word) => {
-    let vowCluster = "", conCluster = "";
-    for (let i = 0; i < word.length; i++) {
-        console.log(word[i]);
-        for (let j = 0; j < vowels.length; j++) {
-            if (word[i] === vowels[j]) {
-                console.log(`${word[i]} = ${vowels[j]}`);
-                vowCluster += word[i];
+    for (let i = 0; i < consonants.length; i++) {
+        if (word[0] === consonants[i]) {
+            isConsonant = true;
+        };
+    };
+
+    if (isVowel) {
+        pigLatinWord = word + "yay";
+    } else if (isConsonant) {
+        for (let i = 0; i < word.length; i++) {
+            for (let j = 0; j < consonants.length; j++) {
+                if (word[i] === consonants[j]) {
+                    console.log(word[i]);
+                };
             };
         };
+        console.log(conCluster);
     };
 
-    if (word)
-
-    console.log(vowCluster);
-
-    // return word;
+    // console.log(vowCluster);
+    return pigLatinWord;
 };
 
 // console.log(convert("say"));
-console.log(isVowel("t"));
 // console.log(convert("english"));
 // console.log(convert("smile"));
+// console.log(convert("airdrop"));
+// console.log(convert("under"));
+
+console.log(convert("smile"));
