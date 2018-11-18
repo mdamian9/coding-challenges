7cd26ca34d1b44a4e4fb5fa64bc177be1cadc256
mdamian9 <m.damian9@hotmail.com>
2018-11-18 15:10:10 -0800

    Added functions isVowel and isConsonant back, rearranged code

--- PigLatinConverter.js
+++ PigLatinConverter.js
@@ -1,32 +1,34 @@
 const vowels = ["a", "e", "i", "o", "u", "y"];
 const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"];
 
+isVowel = (letter) => {
+    let flag = false;
+    for (let i = 0; i < vowels.length; i++) {
+        if (letter === vowels[i]) {
+            flag = true;
+        };
+    };
+    return flag;
+};
+
+isConsonant = (letter) => {
+    let flag = false;
+    for (let i = 0; i < consonants.length; i++) {
+        if (letter === consonants[i]) {
+            flag = true;
+        };
+    };
+    return flag;
+};
+
 convert = (word) => {
 
-    let isVowel, isConsonant, vowCluster = "", conCluster = "", pigLatinWord = "";
-    for (let i = 0; i < vowels.length; i++) {
-        if (word[0] === vowels[i]) {
-            isVowel = true;
-        };
-    };
+    let vowCluster = "", conCluster = "", pigLatinWord = "";
 
-    for (let i = 0; i < consonants.length; i++) {
-        if (word[0] === consonants[i]) {
-            isConsonant = true;
-        };
-    };
-
-    if (isVowel) {
+    if (isVowel(word[0])) {
         pigLatinWord = word + "yay";
-    } else if (isConsonant) {
-        for (let i = 0; i < word.length; i++) {
-            for (let j = 0; j < consonants.length; j++) {
-                if (word[i] === consonants[j]) {
-                    console.log(word[i]);
-                };
-            };
-        };
-        console.log(conCluster);
+    } else if (isConsonant(word[0])) {
+
     };
 
     // console.log(vowCluster);
@@ -34,9 +36,9 @@ convert = (word) => {
 };
 
 // console.log(convert("say"));
-// console.log(convert("english"));
+console.log(convert("english"));
 // console.log(convert("smile"));
-// console.log(convert("airdrop"));
-// console.log(convert("under"));
+console.log(convert("airdrop"));
+console.log(convert("under"));
 
-console.log(convert("smile"));
+// console.log(convert("smile"));
