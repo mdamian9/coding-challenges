14b3513cd3e1bc3e401f3b897ecb0c1cba58c2db
mdamian9 <m.damian9@hotmail.com>
2018-11-18 15:27:55 -0800

    Finished convert function, tested different words, output is correct

--- PigLatinConverter.js
+++ PigLatinConverter.js
@@ -22,23 +22,35 @@ isConsonant = (letter) => {
 };
 
 convert = (word) => {
-
     let vowCluster = "", conCluster = "", pigLatinWord = "";
-
     if (isVowel(word[0])) {
         pigLatinWord = word + "yay";
     } else if (isConsonant(word[0])) {
-
+        let breakLoop = false;
+        for (let i = 0; i < word.length; i++) {
+            for (let j = 0; j < vowels.length; j++) {
+                if (word[i] === vowels[j]) {
+                    breakLoop = true;
+                    vowCluster = word.slice(i);
+                    conCluster = word.slice(0, i);
+                    break;
+                };
+            };
+            if (breakLoop) {
+                break;
+            };
+        };
+        pigLatinWord = vowCluster + conCluster + "ay";
+        console.log(vowCluster);
+        console.log(conCluster);
     };
-
-    // console.log(vowCluster);
     return pigLatinWord;
 };
 
-// console.log(convert("say"));
+console.log(convert("say"));
+console.log(convert("croatia"));
 console.log(convert("english"));
-// console.log(convert("smile"));
+console.log(convert("smile"));
 console.log(convert("airdrop"));
 console.log(convert("under"));
-
-// console.log(convert("smile"));
+console.log(convert("smile"));
