b77c697d9d0fa2a7ad7c308c6d21f39dcb88d554
mdamian9 <m.damian9@hotmail.com>
2018-11-19 15:31:09 -0800

    Started SimpleArraySum.js

---
+++ SimpleArraySum.js
@@ -0,0 +1,10 @@
+// Given an array of integers, find the sum of its elements.
+// Example, if an array arr = [1, 2, 3], 1 + 2 + 3 = 6, so return 6.
+
+sumArr = (arr) => {
+
+};
+
+const arrOne = [2, 4, 6, 8, 10];
+const arrTwo = [11, 23, 7, 9, 10];
+const arrThree = [1, 2, 5, -3, 9];
