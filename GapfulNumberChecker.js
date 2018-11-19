1081137c977788ffffb1e60dfb4104e2156d12a0
mdamian9 <m.damian9@hotmail.com>
2018-11-18 19:18:42 -0800

    Completed function, tested a number, tested a range of numbers. Output is correct

--- GapfulNumberChecker.js
+++ GapfulNumberChecker.js
@@ -1,7 +1,20 @@
 gapfulNumCheck = (number) => {
+    let flag = false;
     var numStr = number.toString();
-    const divisor = parseInt(numStr[0] + numStr[numStr.length - 1]);
-    console.log(divisor);
+    if (numStr.length >= 3) {
+        const divisor = parseInt(numStr[0] + numStr[numStr.length - 1]);
+        // console.log(divisor);
+        if (number % divisor === 0) {
+            flag = true;
+        };
+    };
+    return flag;
 };
 
-gapfulNumCheck(1929);
+console.log(gapfulNumCheck(583));
+
+for (let i = 0; i <= 1000; i++) {
+    if (gapfulNumCheck(i)) {
+        console.log(i);
+    };
+};
