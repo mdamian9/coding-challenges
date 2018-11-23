991d8bba46425afa4d7e5354f26904bb199acf47
mdamian9 <m.damian9@hotmail.com>
2018-11-23 13:30:45 -0800

    Fixed small error in problem statement (comments), completed checkTriangle function, tested values. Output is correct

--- TriangleChecker.js
+++ TriangleChecker.js
@@ -1,10 +1,14 @@
 // Given three integer values, check if a triangle can be constructed with those values of given length.
-// Example: arr = [3, 4, 5], output: true. arr = [10, 20, 9], output true. arr = [7, 9, 1], output: false.
+// Example: arr = [3, 4, 5], output: true. arr = [10, 2, 9], output true. arr = [7, 9, 1], output: false.
 
 checkTriangle = (a, b, c) => {
-
+    let flag = false;
+    if (a + b > c && b + c > a && c + a > b) {
+        flag = true;
+    };
+    return flag;
 };
 
 console.log(checkTriangle(3, 4, 5));
-console.log(checkTriangle(10, 20, 9));
+console.log(checkTriangle(10, 2, 9));
 console.log(checkTriangle(7, 9, 1));
