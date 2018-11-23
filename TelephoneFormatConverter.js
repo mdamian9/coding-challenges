a37bd81bd10386794135bf97a39de08c7d552067
mdamian9 <m.damian9@hotmail.com>
2018-11-23 13:36:20 -0800

    Started on Telephone Format Converter problem, created TelephoneFormatConverter.js

---
+++ TelephoneFormatConverter.js
@@ -0,0 +1,9 @@
+// Write a program that converts an integer input to a telephone number format.
+// Example - Input: 1234567890, Output: (123) 456-7890. Input: 37494455172, Output: (+374) 94-455-172.
+
+convertTelFormat = (num) => {
+
+};
+
+console.log(convertTelFormat(1234567890));
+console.log(convertTelFormat(37494455172));
