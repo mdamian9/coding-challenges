485b79ce89ce01ac8df0d9e69aaa258cee17a6f3
mdamian9 <m.damian9@hotmail.com>
2018-11-22 19:13:09 -0800

    Completed calcRatio function / tested function with different arrays, output is correct

--- PosNegArrRatio.js
+++ PosNegArrRatio.js
@@ -2,7 +2,23 @@
 // Print the decimal value of each fraction on a new line.
 
 calcRatio = (arr) => {
-
+    let pos = 0, neg = 0, zero = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            pos++;
+        } else if (arr[i] < 0) {
+            neg++;
+        } else {
+            zero++;
+        };
+    };
+    const posRatio = pos / arr.length, negRatio = neg / arr.length, zeroRatio = zero / arr.length;
+    return `Positive = ${posRatio.toFixed(6)}\nNegative = ${negRatio.toFixed(6)}\nZero = ${zeroRatio.toFixed(6)}`;
 };
 
-// console.log(calcRatio(arr));
+const arrOne = [1, 1, 0, -1, -1];
+const arrTwo = [0, 0, 0, 0, 1, -1];
+const arrThree = [2, -56, 0, 0, 5, 7, 0, 9, -2, 0];
+console.log(calcRatio(arrOne));
+console.log(calcRatio(arrTwo));
+console.log(calcRatio(arrThree));
