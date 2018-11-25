e6b5b5201c4d031076395b30b103851bc3fee641
mdamian9 <m.damian9@hotmail.com>
2018-11-24 16:33:52 -0800

    Started on Disarium Number Checker problem. Created DisariumNumberChecker.js

---
+++ DisariumNumberChecker.js
@@ -0,0 +1,11 @@
+// A number is called a Disarium number if the sum of the powers of its digits equals the sum of the number itself.
+// The digits are powered to their positions in the number.
+// Example - Input: 135, Output: true. 
+// 135 is a Disarium number because it equals to 1^1 + 3^2 + 5^3 (each digit powered to the position in the number).
+// Write a program to check if a number is a Disarium number or not.
+
+checkDisarium = (num) => {
+
+};
+
+console.log(checkDisarium(135));
