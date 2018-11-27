d78c359c30eb4c74a8c9d562f6e1d673e03f5652
mdamian9 <m.damian9@hotmail.com>
2018-11-26 19:34:09 -0800

    Started on Smallest and Largest in Array problem. Created SmallestLargestInArray.js, set up functions

---
+++ SmallestLargestInArray.js
@@ -0,0 +1,14 @@
+// Write a program that has 2 separate functions that will find the smallest and largest elements in an array.
+// Example - arr = [1, 2, 3, 4], findSmallest(arr) outputs 1 and findLargest(arr) outputs 4.
+
+findSmallest = (arr) => {
+    
+};
+
+findLargest = (arr) => {
+
+};
+
+const arrOne = [1, 2, 3, 4];
+console.log(findSmallest(arrOne));
+console.log(findLargest(arrOne));
