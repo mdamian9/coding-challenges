8812333e6caeaf50e6b03d65fb41c1d8ce49d065
mdamian9 <m.damian9@hotmail.com>
2018-11-26 19:36:18 -0800

    Completed findLargest function in SmallestLargestInArray.js

--- SmallestLargestInArray.js
+++ SmallestLargestInArray.js
@@ -2,11 +2,17 @@
 // Example - arr = [1, 2, 3, 4], findSmallest(arr) outputs 1 and findLargest(arr) outputs 4.
 
 findSmallest = (arr) => {
-    
+
 };
 
 findLargest = (arr) => {
-
+    let largest = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i]
+        };
+    };
+    return largest;
 };
 
 const arrOne = [1, 2, 3, 4];
