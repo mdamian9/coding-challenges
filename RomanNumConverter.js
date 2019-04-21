e2febeb1254ca67f2197f4d5927046b59dfd8a0f
mdamian9 <m.damian9@hotmail.com>
2019-04-21 10:23:18 -0700

    Completed RomanNumConverter algorithm

--- RomanNumConverter.js
+++ RomanNumConverter.js
@@ -1,4 +1,13 @@
 convertToRoman = (num) => {
-    let convertedNum = 0;
-    return convertedNum = "";
+    let convertedNum = "";
+    const numerals = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
+    for (let i in numerals) {
+        while (num >= numerals[i]) {
+            convertedNum += i;
+            num -= numerals[i];
+        };
+    };
+    return convertedNum;
 };
+
+console.log(convertToRoman(14));
